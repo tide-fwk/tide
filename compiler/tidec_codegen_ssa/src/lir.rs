585ad0468375f9a0009043668ec7fe744fde7565
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2025-05-26 22:33:32 +0200

    add(lib/abi)[mnt]: documentation for basic `struct`s
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- compiler/tidec_codegen_ssa/src/lir.rs
+++ compiler/tidec_codegen_ssa/src/lir.rs
@@ -1,7 +1,7 @@
-use tidec_abi::calling_convention::function::FnAbi;
+use tidec_abi::{Align, TyAndLayout, calling_convention::function::FnAbi};
 use tidec_lir::{
     lir::LirBody,
-    syntax::{Local, LocalData},
+    syntax::{LirTy, Local, LocalData},
 };
 use tidec_utils::index_vec::IdxVec;
 
@@ -9,6 +9,47 @@ use crate::{
     entry::FnCtx,
     traits::{BuilderMethods, CodegenMethods},
 };
+
+/// A reference to a place in memory during code generation, used in LIR to backend lowering.
+///
+/// `PlaceRef` encapsulates both the backend representation of the place (e.g., a value)
+/// and its type layout information. This is used to generate correct code when accessing, modifying,
+/// or analyzing memory locations.
+///
+/// The type parameter `V` represents the backend-specific value, such as an LLVM value or a
+/// machine register in custom backends.
+pub struct PlaceRef<V> {
+    /// The backend value and alignment for this place.
+    ///
+    /// This typically holds a pointer or immediate value, along with alignment metadata.
+    place_value: PlaceVal<V>,
+    /// The type and layout of the place, used for determining ABI, size, and alignment.
+    ///
+    /// This is essential for correct code generation, especially for aggregates, unsized types,
+    /// or types with nontrivial ABI requirements.
+    place_ty_layout: TyAndLayout<LirTy>,
+}
+// impl<V: Copy + PartialEq + std::fmt::Debug> PlaceRef<V> {}
+
+/// A backend value paired with alignment information, representing the underlying storage
+/// for a LIR place during codegen.
+///
+/// This struct abstracts over how a place is represented in the backend,
+/// whether it be a memory address, an SSA value, or other representations.
+///
+/// This is tipically used in conjunction with [`PlaceRef`].
+///
+/// The type parameter `V` is the backend-specific representation of values.
+pub struct PlaceVal<V> {
+    /// The actual backend value for this place (e.g., pointer, immediate, etc.).
+    value: V,
+    /// Alignment of the value in memory.
+    ///
+    /// This is used to ensure proper access semantics and may affect how code is emitted,
+    /// especially for aligned loads/stores and optimizations.
+    align: Align,
+}
+// impl<V: Copy + PartialEq + std::fmt::Debug> PlaceVal<V> {}
 
 pub fn compile_lir_body<'a, 'be, B: BuilderMethods<'a, 'be>>(
     ctx: &'a B::CodegenCtx,
